6d85ceac0c7e558bc063b40137cf1214bfe21472
marcelino pangilinan <46616841+marbysneaker@users.noreply.github.com>
2022-08-30 22:50:46 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+#include <vector>
+
+// Example 1: 
+// Input: nums = [2,7,11,15], target = 9 
+// Output: [0,1] 
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]. 
+
+vector<int> twoSum(vector<int> nums; int target){
+    for(int i =0; i< nums.size(); i++){
+        for(int j = 1;nums.size(); j++){
+            if(nums[i] + nums[j] == target){
+                return {i, j};
+            }
+        }
+    }
+    return {-1,-1}
+}
+
+
+int main(){
+  
+}
