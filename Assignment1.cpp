9aa786032374a914e59da871ceb7f8c9011ba934
marcelino pangilinan <46616841+marbysneaker@users.noreply.github.com>
2022-08-31 02:56:00 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -13,7 +13,7 @@ using namespace std;
 // Output: [0,1] 
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]. 
 
-std::vector<int> twoSum(std::vector<int> nums, int target) {
+vector<int> twoSum(vector<int> nums, int target) {
     vector<int> result;
     for (int i = 0; i < nums.size(); i++) {
         for (int j = 1; nums.size(); j++) {
@@ -27,6 +27,54 @@ std::vector<int> twoSum(std::vector<int> nums, int target) {
     return result;
 }
 
+// 3)  Median of Two Sorted Arrays 
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the 
+// two sorted arrays. The overall run time complexity should be O(log (m+n)). 
+//  Example 1: 
+// Input: nums1 = [1,3], nums2 = [2] 
+// Output: 2.00000 
+// Explanation: merged array = [1,2,3] and median is 2. 
+// Example 2: 
+// Input: nums1 = [1,2], nums2 = [3,4] 
+// Output: 2.50000 
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5. 
+
+ double findMedianSortedArrays(vector<int> nums1, vector<int> nums2) {
+        int i = 0, j =0;
+        int n1 = nums1.size(), n2 =nums2.size();
+        vector<int>nums3;
+        
+        while(i<n1 && j< n2){
+            
+            if(nums1[i]<=nums2[j]){
+                
+                nums3.push_back(nums1[i]);
+                i++;
+                
+            }
+            else{
+                
+                nums3.push_back(nums2[j]);
+                j++;
+            }
+        }
+        
+        while(i<n1){
+            nums3.push_back(nums1[i]);
+            i++;
+            
+        }
+        while(j<n2){
+            nums3.push_back(nums2[j]);
+            j++;
+        }
+        cout<<nums3.size()<<endl;
+        int mid = nums3.size()/2;
+       if(nums3.size() % 2 == 0){
+           return ((nums3[mid]+nums3[mid-1])/2.0);
+       }
+        return(nums3[mid]);
+    }
 
 
 int main(){
