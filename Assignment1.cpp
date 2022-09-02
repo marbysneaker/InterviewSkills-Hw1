7e61cc4f1a33b2911780543d76220a8d0e4fcac4
marcelino pangilinan <46616841+marbysneaker@users.noreply.github.com>
2022-09-01 19:39:47 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -27,6 +27,53 @@ vector<int> twoSum(vector<int> nums, int target) {
     return result;
 }
 
+// 2)  Find First and Last Position of Element in Sorted Array. 
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending 
+// position of a given target value. If target is not found in the array, return [-1, -1]. 
+// You must write an algorithm with O(log n) runtime complexity. 
+//  Example 1: 
+// Input: nums = [5,7,7,8,8,10], target = 8 
+// Output: [3,4] 
+// Example 2: 
+// Input: nums = [5,7,7,8,8,10], target = 6 
+// Output: [-1,-1] 
+// Example 3: 
+// Input: nums = [], target = 0 
+
+
+vector<int> searchRange(vector<int>& nums, int target) {
+        
+        int low = 0, high = nums.size();
+        vector<int>result;
+        if(nums.empty()){
+             return{-1,-1};
+         }
+        if(nums.size()<2 && target == nums[0]){
+            return{0,0};
+        }
+        
+        
+        while(low<high){
+           int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target){
+              result.push_back(mid);
+              while(nums[mid] == target){
+                  mid++;
+              }
+              result.push_back(--mid);
+              return result;
+            }
+
+            if (nums[mid] < target)
+              low = mid + 1;
+
+            else
+              high = mid - 1;
+        }
+        return{-1,-1};
+    }
+
 // 3)  Median of Two Sorted Arrays 
 // Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the 
 // two sorted arrays. The overall run time complexity should be O(log (m+n)). 
